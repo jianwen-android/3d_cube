45ea5378123d3a1b483f8ddf711215de51e0b7f9
Jian Wen <jianwen815@gmail.com>
2026-03-02 18:00:48 +0800

    found new example code on MDN sources, will look into frame-by-frame drawing for the purpose of mediarecorder

--- record.js
+++ record.js
@@ -1,28 +1,44 @@
-const canvas = document.getElementById("game"); // Replace 'myCanvas' with your canvas ID
-const stream = canvas.captureStream(120); // 120 FPS
+window.onload = () => {
+  var canvas = document.getElementById("game"); // Replace 'myCanvas' with your canvas ID
+  var stream = canvas.captureStream(120); // 120 FPS
+  var capturing = false;
 
-// if (MediaRecorder.isTypeSupported("video/webm;codecs=vp9")) {
-//   const MEDIA = new MediaRecorder(stream, {
-//     mimeType: "video/webm;codecs=vp9",
-//   });
-//   console.log("MEDIA init");
-// }
+  var MEDIA = new MediaRecorder(stream, {
+    mimeType: "video/webm",
+  });
+  console.log("MEDIA init");
 
-const MEDIA = new MediaRecorder(stream, {
-  mimeType: "video/webm;codecs=opus,vp8",
-});
-console.log("MEDIA init");
+  recorder.addEventListener("dataavailable", exportVideo);
+
+  // To start recording:
+  MEDIA.start();
+  console.log("MEDIA START");
+  // To stop recording after 4s
+  setTimeout(() => MEDIA.stop(), 4000);
+  console.log("MEDIA STOP");
+
+  MEDIA.ondataavailable = saveChunks;
+  MEDIA.onstop = exportVideo;
+};
+
+function startCapturing
 
 let chunks = [];
-MEDIA.ondataavailable = (e) => {
+
+function saveChunks(e) {
   if (e.data.size > 0) {
     chunks.push(e.data);
   }
-};
+}
 
-function loadChunksIntoFile() {
-  const blob = new Blob(chunks, { type: "video/webm" });
+function exportVideo(e) {
+  var videoData = [e.date];
+  var blob = new Blob(videoData, { type: MEDIA.mimeType });
   const recordedVideoUrl = URL.createObjectURL(blob);
+
+  var vid = document.getElementById("videoPlayer");
+  vid.src = recordedVideoUrl;
+
   const a = document.createElement("a");
   a.href = recordedVideoUrl;
   a.download = "recorded-canvas.webm";
@@ -30,17 +46,6 @@ function loadChunksIntoFile() {
   a.click();
   URL.revokeObjectURL(recordedVideoUrl); // Clean up the object URL
 }
-
-MEDIA.onstop = loadChunksIntoFile();
-
-// To start recording:
-MEDIA.start();
-console.log("MEDIA START");
-// To stop recording after some time 4
-setTimeout(() => {
-  MEDIA.stop();
-}, 4000);
-console.log("MEDIA STOP");
 
 // function createFileFromCurrentRecordedData() {
 //   const file = new File(recordedData, "recording.webm", { type: "video/webm" });
